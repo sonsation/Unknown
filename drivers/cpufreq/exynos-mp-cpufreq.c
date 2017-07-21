c45fabcb6ed8698fc4013d526675c2cf08a319e9
sonsation <jhson9304@gmail.com>
2017-07-21 22:28:49 +0900

    cpufreq : Updated for UC

--- drivers/cpufreq/exynos-mp-cpufreq.c
+++ drivers/cpufreq/exynos-mp-cpufreq.c
@@ -72,7 +72,7 @@
 #ifdef CONFIG_SOC_EXYNOS7420
 #define CL0_MIN_FREQ		400000
 #define CL0_MAX_FREQ		1600000
-#define CL1_MIN_FREQ		800000
+#define CL1_MIN_FREQ		400000
 #define CL1_MAX_FREQ		2200000
 #else
 #error "Please define core frequency ranges for current SoC."
