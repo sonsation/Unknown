cd300f6889042df0093da180ddd81fc2214c8980
sonsation <jhson9304@gmail.com>
2017-07-21 20:36:52 +0900

    Ramdisk : Removed unnecessary files(1)

--- ramdisk/SM-G920F/ramdisk/sbin/sysinit.sh
+++ ramdisk/SM-G920F/ramdisk/sbin/sysinit.sh
@@ -10,15 +10,6 @@ else
 fi;
 
 #####################################################################
-# Mount root as RW to apply tweaks and settings
-if [ "$($BB mount | grep rootfs | cut -c 26-27 | grep -c ro)" -eq "1" ]; then
-	$BB mount -o remount,rw /;
-fi;
-if [ "$($BB mount | grep system | grep -c ro)" -eq "1" ]; then
-	$BB mount -o remount,rw /system;
-fi;
-
-#####################################################################
 # Make Kernel Data Path
 
 if [ ! -d /data/.unknown ]; then
@@ -36,19 +27,6 @@ if [ ! -e /system/etc/init.d ]; then
 	chown -R root.root /system/etc/init.d
 	chmod -R 755 /system/etc/init.d
 fi
-
-#####################################################################
-# Synapse
-$BB chmod -R 755 /res/*
-$BB ln -fs /res/synapse/uci /sbin/uci
-/sbin/uci
-
-if [ "$($BB mount | grep rootfs | cut -c 26-27 | grep -c ro)" -eq "1" ]; then
-	$BB mount -o remount,rw /;
-fi;
-if [ "$($BB mount | grep system | grep -c ro)" -eq "1" ]; then
-	$BB mount -o remount,rw /system;
-fi;
 
 #####################################################################
 # start init.d
