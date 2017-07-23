b13adbf762860504ebe1c241365802961b505a9e
sonsation <jhson9304@gmail.com>
2017-07-23 21:04:56 +0900

    cpufreq : Add missing arguments

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -461,7 +461,7 @@ static void cpufreq_bL_allstats_free(void)
 }
 
 static int cpufreq_stats_create_table(struct cpufreq_policy *policy,
-		struct cpufreq_frequency_table *table)
+		struct cpufreq_frequency_table *table, int count)
 {
 	unsigned int i, j, ret = 0;
 	struct cpufreq_stats *stat;
