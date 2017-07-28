907d16c9ae31e4abd98657a67f3079d60f0ef136
sonsation <jhson9304@gmail.com>
2017-07-28 17:16:30 +0900

    Revert.panic: shut the fuck up Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com> (reverted from commit 5e4144701c85003a21dbada694db9007bac7cbe0)

--- kernel/panic.c
+++ kernel/panic.c
@@ -123,6 +123,7 @@ void panic(const char *fmt, ...)
 	printk(KERN_EMERG "Kernel panic - not syncing: %s\n",buf);
 
 	exynos_ss_prepare_panic();
+	exynos_ss_dump_panic();
 #ifdef CONFIG_DEBUG_BUGVERBOSE
 	/*
 	 * Avoid nested stack-dumping if a panic occurs during oops processing
@@ -131,6 +132,15 @@ void panic(const char *fmt, ...)
 		dump_stack();
 #endif
 
+#if defined(CONFIG_SOC_EXYNOS5422) || defined(CONFIG_SOC_EXYNOS5430)
+	show_exynos_pmu();
+#endif
+#if defined(CONFIG_SOC_EXYNOS5422) || defined(CONFIG_SOC_EXYNOS5430)
+	show_exynos_cmu();
+#endif
+#if CONFIG_SCHED_DEBUG
+	sysrq_sched_debug_show();
+#endif
 	/*
 	 * If we have crashed and we have a crash kernel loaded let it handle
 	 * everything else.
@@ -146,6 +156,8 @@ void panic(const char *fmt, ...)
 	smp_send_stop();
 
 	kmsg_dump(KMSG_DUMP_PANIC);
+
+	exynos_cs_show_pcval();
 
 	atomic_notifier_call_chain(&panic_notifier_list, 0, buf);
 
