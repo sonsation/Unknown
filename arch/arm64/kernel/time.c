c1a7206d894f323383f273aaa77c48dd3cf95cc2
Lorenzo Pieralisi <Lorenzo.Pieralisi@arm.com>
2017-08-24 03:40:52 +0900

    arm64: kernel: initialize broadcast hrtimer based clock event device On platforms implementing CPU power management, the CPUidle subsystem can allow CPUs to enter idle states where local timers logic is lost on power down. To keep the software timers functional the kernel relies on an always-on broadcast timer to be present in the platform to relay the interrupt signalling the timer expiries.
    For platforms implementing CPU core gating that do not implement an always-on
    HW timer or implement it in a broken way, this patch adds code to initialize
    the kernel hrtimer based clock event device upon boot (which can be chosen as
    tick broadcast device by the kernel).
    It relies on a dynamically chosen CPU to be always powered-up. This CPU then
    relays the timer interrupt to CPUs in deep-idle states through its HW local
    timer device.
    
    Having a CPU always-on has implications on power management platform
    capabilities and makes CPUidle suboptimal, since at least a CPU is kept
    always in a shallow idle state by the kernel to relay timer interrupts,
    but at least leaves the kernel with a functional system with some working
    power management capabilities.
    
    The hrtimer based clock event device is unconditionally registered, but
    has the lowest possible rating such that any broadcast-capable HW clock
    event device present will be chosen in preference as the tick broadcast
    device.
    
    Reviewed-by: Preeti U Murthy <preeti@linux.vnet.ibm.com>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    (cherry picked from commit 9358d755bd5cba8965ea79f2a446e689323409f9,
     again after temporary revert)
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/kernel/time.c
+++ arch/arm64/kernel/time.c
@@ -123,6 +123,8 @@ void __init time_init(void)
 	of_clk_init(NULL);
 	clocksource_of_init();
 
+        tick_setup_hrtimer_broadcast();  
+
 	arch_timer_rate = arch_timer_get_rate();
 	if (!arch_timer_rate)
 		panic("Unable to initialise architected timer.\n");
