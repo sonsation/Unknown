62da73e47494e8056c5d57adff8afe7186c33468
Lukas0610 <mail@lukasberger.at>
2017-10-02 22:46:22 +0900

    moved android-drivers out of staging

--- drivers/video/exynos/decon/decon_core.c
+++ drivers/video/exynos/decon/decon_core.c
@@ -55,7 +55,7 @@
 #include "dsim.h"
 #include "decon_helper.h"
 #include "mdnie_reg.h"
-#include "../../../staging/android/sw_sync.h"
+#include "../../../android/sw_sync.h"
 #include "vpp/vpp_core.h"
 
 #define SUCCESS_EXYNOS_SMC	2
