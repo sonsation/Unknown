44126eecf322029c93c915c4ab69e92a51ea2c48
sonsation <jhson9304@gmail.com>
2017-10-09 05:18:23 +0900

    kernel:fixing gentle_fair_sleepers to convert and enable arch_power by default

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2057,7 +2057,7 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 }
 
 static unsigned int Lgentle_fair_sleepers = 0;
-static unsigned int Larch_power = 0;
+static unsigned int Larch_power = 1;
 
 void relay_gfs(unsigned int gfs)
 {
@@ -2091,7 +2091,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 		 * Halve their sleep time's effect, to allow
 		 * for a gentler effect of sleepers:
 		 */
-		if (sched_feat(GENTLE_FAIR_SLEEPERS))
+		if (Lgentle_fair_sleepers)
 			thresh >>= 1;
 
 		vruntime -= thresh;
