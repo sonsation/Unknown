0134bf3d0c6f72df715c037b49f9d9e79234c89b
sonsation <jhson9304@gmail.com>
2017-10-28 01:55:47 +0900

    ramdisk : update

--- ramdisk/SM-G920F/ramdisk/init.service.sh
+++ ramdisk/SM-G920F/ramdisk/init.service.sh
@@ -2,16 +2,8 @@
 
 on property:sys.boot_completed=1
     start sysinit
-    start wakelock
 
 service sysinit /sbin/sysinit.sh
-    class late_start
-    user root
-    seclabel u:r:init:s0
-    oneshot
-    disabled
-
-service wakelock /sbin/wakelock.sh
     class late_start
     user root
     seclabel u:r:init:s0
